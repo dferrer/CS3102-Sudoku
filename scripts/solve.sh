08a5a7b7d8f65934ff599c7b38017e2fcb40b6a3
Dylan <dferrer427@gmail.com>
2014-04-28 21:43:26 -0400

    Updated solve script

--- scripts/solve.sh
+++ scripts/solve.sh
@@ -1,16 +1,11 @@
-go build main.go
 START=$(date +%s)
 INDEX=0
-# parallel --gnu --jobs 0 ./main ::: puzzles/big/formatted/* #> ${p//formatted/solved}
-# for p in puzzles/[^12]*/formatted/*
-for p in puzzles/[^12]*/formatted/*
+for p in puzzles/$1/formatted/*
 do
-	# echo "$p"
-	./main $p & #> ${p//formatted/solved}
+	./main $p > ${p//formatted/solved} &
 	INDEX=$[$INDEX + 1]
 done
 wait
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Solved $INDEX puzzles in $DIFF seconds."
-rm main
+echo "Solved $INDEX $2 puzzles in $DIFF seconds."
